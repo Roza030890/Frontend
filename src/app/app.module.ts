8640aa906fee8f42f6164013b414976b633bc5d3
Roza030890 <roza030890@gmail.com>
2022-06-16 21:12:53 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { FoodComponent } from './food/food.component';
 import { BeverageComponent } from './beverage/beverage.component';
 import { OffersComponent } from './offers/offers.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
-
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 // import { HttpClientModule} from '@angular/common/http';
-
 
 @NgModule({
   declarations: [
